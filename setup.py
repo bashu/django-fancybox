f77fb644e0a9616d780c041d0cc419e498492fd0
Basil Shubin <basil.shubin@gmail.com>
2013-08-01 19:11:18 +0700

    initial commit

---
+++ setup.py
@@ -0,0 +1,31 @@
+import os
+from setuptools import setup
+
+README = open(os.path.join(os.path.dirname(__file__), 'README.rst')).read()
+
+# allow setup.py to be run from any path
+os.chdir(os.path.normpath(os.path.join(os.path.abspath(__file__), os.pardir)))
+
+setup(
+    name='django-colorbox',
+    version='0.1',
+    packages=['colorbox'],
+    include_package_data=True,
+    license='BSD License',
+    description='Put short description here...',
+    long_description=README,
+    url='http://github.com/bashu/django-colorbox',
+    author='Basil Shubin',
+    author_email='basil.shubin@gmail.com',
+    classifiers=[
+        'Environment :: Web Environment',
+        'Framework :: Django',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: BSD License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Topic :: Internet :: WWW/HTTP',
+        'Topic :: Internet :: WWW/HTTP :: Dynamic Content',
+    ],
+    zip_safe=False,
+)
