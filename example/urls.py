58f8f4881a9e97206ddf49ea6cfb7f48dd34bfb3
Basil Shubin <basil.shubin@gmail.com>
2021-11-19 17:28:30 +0700

    Upgrade code to Python 3.6+, Django 2.2 and remove deprecations

--- example/urls.py
+++ example/urls.py
@@ -1,7 +1,7 @@
-from django.conf.urls import url
+from django.urls import path, re_path
 from django.views.generic import TemplateView
 
 urlpatterns = [
-    url(r"^$", TemplateView.as_view(template_name="homepage.html")),
-    url(r"^remote.html$", TemplateView.as_view(template_name="remote.html"), name="remote.html"),
+    path('', TemplateView.as_view(template_name="homepage.html")),
+    re_path(r"^remote.html$", TemplateView.as_view(template_name="remote.html"), name="remote.html"),
 ]
