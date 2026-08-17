a2ec05badcda7108685c42ec48a8ecccaf487586
Basil Shubin <144783+bashu@users.noreply.github.com>
2026-08-17 15:26:25 +0600

    Simplify test settings

--- fancybox/tests/settings.py
+++ fancybox/tests/settings.py
@@ -5,30 +5,10 @@ SECRET_KEY = "DUMMY_SECRET_KEY"  # noqa: S105
 PROJECT_APPS = ["fancybox"]
 
 INSTALLED_APPS = [
-    "django.contrib.contenttypes",
-    "django.contrib.auth",
-    "django.contrib.staticfiles",
     *PROJECT_APPS,
-]
-
-TEMPLATES = [
-    {
-        "BACKEND": "django.template.backends.django.DjangoTemplates",
-        "APP_DIRS": True,
-        "OPTIONS": {
-            "context_processors": [
-                "django.template.context_processors.request",
-            ],
-        },
-    },
 ]
 
 # Database
 # https://docs.djangoproject.com/en/1.8/ref/settings/#databases
 
 DATABASES = {"default": {"ENGINE": "django.db.backends.sqlite3", "NAME": ":memory:"}}
-
-# Static files (CSS, JavaScript, Images)
-# https://docs.djangoproject.com/en/1.8/howto/static-files/
-
-STATIC_URL = "/static/"
